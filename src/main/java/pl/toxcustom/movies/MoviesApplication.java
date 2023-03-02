42d548193276a557f0a4e2032486de9dfa5e3660
toXCustom <pawel89@gmail.com>
2023-03-02 15:19:09 +0100

    Movies and Reviews Endpoints

--- src/main/java/pl/toxcustom/movies/MoviesApplication.java
+++ src/main/java/pl/toxcustom/movies/MoviesApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
